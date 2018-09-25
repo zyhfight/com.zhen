29b9678ea367577a600da1e767d080c0ef803a76
80234697 <80234697@itc.cmbchina.cn>
2018-09-25 09:19:37 +0800

    update

--- questionnaireProject/src/ServiceInterface/userInterface.java
+++
@@ -1,9 +0,0 @@
-package ServiceInterface;
-
-import java.util.List;
-
-public interface userInterface {
-	public boolean addUser(Object obj);
-	public List<Object> selectUser(String sql,Object obj);
-	public boolean deleteUser(String str);
-}
--- questionnaireProject/src/ServiceInterface/usrInterface.java
+++
@@ -1,8 +0,0 @@
-package ServiceInterface;
-
-public interface usrInterface {
-	public int checkIdentity(String sql);
-	public boolean addUsr(Object obj);
-	public boolean deleteUsr(String str);
-
-}
