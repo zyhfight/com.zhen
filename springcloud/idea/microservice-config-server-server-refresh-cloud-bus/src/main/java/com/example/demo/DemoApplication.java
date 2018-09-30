2b8b91d2a22d6917f9c68d24cec46d82a5e0a259
80234697 <80234697@itc.cmbchina.cn>
2018-09-30 15:15:52 +0800

    修改Spring Cloud Bus自动刷新

--- springcloud/idea/microservice-config-server-server-refresh-cloud-bus/src/main/java/com/example/demo/DemoApplication.java
+++ springcloud/idea/microservice-config-server-server-refresh-cloud-bus/src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+* /refresh端点
+* 发送POST请求到其中一个Config Client实例的/bus/refresh端点
+* eg：http://localhost:8081/bus/refresh
+* 借助Git仓库的WebHooks即可实现配置的自动刷新。
+*/
 @SpringBootApplication
 @EnableConfigServer
 public class DemoApplication {
