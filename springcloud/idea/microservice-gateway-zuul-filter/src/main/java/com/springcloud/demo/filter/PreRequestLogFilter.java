7e431e4ab5421227a8355c6bc59980285e9ce120
80234697 <80234697@itc.cmbchina.cn>
2018-09-30 11:45:15 +0800

    增加zuul 注释

--- springcloud/idea/microservice-gateway-zuul-filter/src/main/java/com/springcloud/demo/filter/PreRequestLogFilter.java
+++ springcloud/idea/microservice-gateway-zuul-filter/src/main/java/com/springcloud/demo/filter/PreRequestLogFilter.java
@@ -17,22 +17,36 @@ public class PreRequestLogFilter extends ZuulFilter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PreRequestLogFilter.class);
 
+	/**
+	* 返回过滤器类型
+	*
+	*/
     @Override
     public String filterType() {
         return FilterConstants.PRE_TYPE;
     }
 
+	/**
+	* 指定过滤器执行顺序
+	* 数值越小，优先级越高
+	*/
     @Override
     public int filterOrder() {
         //在org.springframework.cloud.netflix.zuul.filters.pre.PreDecorationFilter过滤器前执行
         return FilterConstants.PRE_DECORATION_FILTER_ORDER - 1;
     }
 
+	/**
+	* true:执行，false：不执行
+	*/
     @Override
     public boolean shouldFilter() {
         return true;
     }
 
+	/**
+	* 过滤器具体逻辑
+	*/
     @Override
     public Object run() {
 
