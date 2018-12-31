19349a47327d5d26c456e7f243b4edca8ca39761
zyhfight <zhenyonghe@126.com>
2018-12-31 21:03:45 +0800

    Update StaticInnerSingleton.java

--- DesignPatterns/SingletonPattern/StaticInnerSingleton.java
+++ DesignPatterns/SingletonPattern/StaticInnerSingleton.java
@@ -1,12 +1,13 @@
 /**
- * ��̬�ڲ���
+ * 静态内部类
  */
 package DesignPatterns.SingletonPattern;
 /**
- * �ⲿ������ˣ�ʵ����һ������ʼ����ֻ�е���getInstance����ʱ���Ż���ʽ�����ڲ��࣬�Ӷ�����ʵ��
- * ������ʵ���ܺ���Դʱ������ʵ���ӳټ���,�̰߳�ȫ
+ * 外部类加载了，实例不一定被初始化，只有调用getInstance方法时，才会显式加载内部类，从而创建实例
+ * 当创建实例很耗资源时，可以实现延迟加载,线程安全
  * @author zhen
  *
+ * java编程语言特性，很多其他面向对象编程语言不支持；
  */
 public class StaticInnerSingleton {
 	private static class InnerSingleton{
